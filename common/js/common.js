e96a7d4edc292cc2517f553088fb5b8acef5a200
Index-Kanematsu <kanematsu@index-media.jp>
2022-10-18 10:24:16 +0900

    first commit

---
+++ common/js/common.js
@@ -0,0 +1,59 @@
+// FadeIn Animation
+jQuery(function () {
+	const fadeIn = jQuery('.js-fade-in');
+	jQuery(window).on('scroll', function () {
+		jQuery(fadeIn).each(function () {
+			var offset = jQuery(this).offset().top;
+			var scroll = jQuery(window).scrollTop();
+			var windowHeight = jQuery(window).height();
+			if (scroll > offset - windowHeight + 150) {
+				jQuery(this).addClass("js-scroll-in");
+			}
+		});
+	});
+});
+
+// Tab Contents
+jQuery(function () {
+	jQuery('.js-tabSelect li').click(function () {
+		var index = jQuery('.js-tabSelect li').index(this);
+		jQuery('.js-tabSelect li').removeClass('active');
+		jQuery(this).addClass('active');
+		jQuery('.js-tabContent').removeClass('show').eq(index).addClass('show');
+	});
+});
+
+
+// Header -------------------------
+// Fixed Header スクロールしたら表示
+const nav = jQuery('.header');
+const offset = nav.offset().top;
+const height = nav.innerHeight(); //ヘッダー高さ
+
+jQuery(function(){
+	jQuery(window).scroll(function(){
+		const scroll = jQuery(this).scrollTop();
+		//ヘッダーの高さを超えたらfixedクラスをつける
+		if(scroll >= height){
+			nav.addClass('fixed');
+			// headerの高さ分余白をつける
+			jQuery('main').css('padding-top', height);
+		} else {
+			nav.removeClass('fixed');
+			jQuery('main').css('padding-top', 0);
+		}
+	})
+});
+
+// For smartphone------------------
+// Humberger Menu
+jQuery(function () {
+	jQuery('.navToggle').click(function () {
+		jQuery(this).toggleClass('active');
+		if (jQuery(this).hasClass('active')) {
+			jQuery('#js-nav').addClass('active');
+		} else {
+			jQuery('#js-nav').removeClass('active');
+		}
+	});
+});
